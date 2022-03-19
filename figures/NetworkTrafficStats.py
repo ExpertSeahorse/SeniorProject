448767793883215f1ad927a722054c382bcd4f72
David <dtfeldman@verizon.net>
2022-03-19 14:58:28 -0400

    First graph working correctly

---
+++ figures/NetworkTrafficStats.py
@@ -0,0 +1,59 @@
+import pandas
+import plotly.graph_objects as go
+import plotly.subplots as sp
+def buildNTS(pan_allow, pan_block):
+    # Sum all counts for a given month
+    sum_allow = pan_allow.groupby('date', as_index=False).sum()
+    sum_block = pan_block.groupby('date', as_index=False).sum()
+
+    # Move all required columns into a single DF and calculate any derived information
+    cols = pandas.DataFrame()
+    cols["date"] = sum_allow['date']
+    cols["allow_ct"] = sum_allow['count']
+    cols["block_ct"] = sum_block['count']
+    cols["prcnt_block"] = cols['block_ct']/(cols['allow_ct']+cols['block_ct'])
+    cols["trnd_block"] = cols['prcnt_block'].expanding().mean()
+    # print(cols)
+
+    # Create all subplots for the first graph
+    figlist = [
+        go.Bar(
+            name="Action=Allowed",
+            x=cols['date'],
+            y=cols['allow_ct'],
+            marker_color="SkyBlue"
+        ),
+        go.Bar(
+            name="Action=Blocked",
+            x=cols['date'],
+            y=cols['block_ct'],
+            marker_color="SteelBlue"
+        ),
+        go.Scatter(
+            name="Percent Blocked",
+            x=cols['date'],
+            y=cols["prcnt_block"],
+            marker_color="CornflowerBlue"
+        ),
+        go.Scatter(
+            name="Avg Blocked",
+            x=cols['date'],
+            y=cols["trnd_block"],
+            marker_color="Chartreuse"
+        )
+    ]
+    # Overlap all the subplots onto one plot
+    fig = sp.make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(figlist[0], secondary_y=False)
+    fig.add_trace(figlist[1], secondary_y=False)
+    fig.add_trace(figlist[2], secondary_y=True)
+    fig.add_trace(figlist[3], secondary_y=True)
+    
+    # Apply formatting
+    fig.update_layout(
+        title = 'Network Traffic Stats​',
+        xaxis_title = 'Dates',
+        yaxis_title = 'Count',
+        barmode='stack'
+    )
+    return fig
--- main.py
+++ main.py
@@ -10,52 +10,24 @@ import os
 import dash
 from dash import html
 from dash import dcc
-import plotly.graph_objects as go
-import plotly.express as px
+
 import pandas
-import numpy as np
 
+# Import csv files into memory
+CSVs = {}
 directory = 'data'
-
-# csvs = []
-# for filename in os.listdir(directory):
-#     csvs.append(pandas.read_csv(filename))
-
-pan_allow = pandas.read_csv(os.path.join(directory, 'pan_allowed_traffic_stats_complete.csv'))
-# print(pan_allow)
-pan_block = pandas.read_csv(os.path.join(directory, 'pan_blocked_traffic_stats_complete.csv'))
+for filename in os.listdir(directory):
+    CSVs[filename] = pandas.read_csv(os.path.join(directory, filename))
 
 def buildGraphs():
-    # sum_allow = pan_allow.groupby('date', as_index=False).sum()
-    # print(sum_allow)
-    # sum_block = pan_block.groupby('date', as_index=False).sum()
-    fig = go.Figure([
-        go.Bar(
-            name="Action=Allowed",
-            x=pan_allow['date'],
-            y=pan_allow['count'],
-            marker_color="SkyBlue"
-        ),
-        go.Bar(
-            name="Action=Blocked",
-            x=pan_block['date'],
-            y=pan_block['count'],
-            marker_color="SteelBlue"
-        )
-    ])
-    fig.update_layout(
-        title = 'Network Traffic Stats​',
-        xaxis_title = 'Dates',
-        yaxis_title = 'Count',
-        barmode='stack'
-    )
-    return fig
+    from figures.NetworkTrafficStats import buildNTS
+    return buildNTS(CSVs["pan_allowed_traffic_stats_complete.csv"], CSVs["pan_blocked_traffic_stats_complete.csv"])
     
 
 app = dash.Dash()   #initialising dash app
 app.layout = html.Div(id = 'parent', children = [
     html.H1(id = 'H1', children = 'Raymond James Dashboard', style = {'textAlign':'center', 'marginTop':40,'marginBottom':40}),
-    dcc.Graph(id = 'line_plot', figure = buildGraphs())    
+    dcc.Graph(id = 'plot', figure = buildGraphs())    
 ])
 
 
