20e9b587aa177b7a7a27f4b4051c041ae482fe63
Luke M <77419443+vader124125@users.noreply.github.com>
2022-03-20 16:01:04 -0400

    Figures C and D

---
+++ figures/TotalNetworkStats.py
@@ -0,0 +1,44 @@
+"""
+c. “Allowed” Threats represented as a table using host_exploit_threat_stats_complete.csv
+"""
+import pandas
+import plotly.graph_objects as go
+import plotly.subplots as sp
+import numpy as np
+def build(threat_stats):
+    # Sum all counts for a given month
+    sum_action = threat_stats.groupby(['time', 'Action'], as_index=False, dropna=False)['count'].sum()
+    # print(sum_action)
+
+    # Move all required columns into a single DF and calculate any derived information
+    cols = pandas.DataFrame()
+    cols["date"] = sum_action['time']
+
+    cols["allowed"] = sum_action[sum_action['Action']=="Blocked"]['count']
+    cols["blocked"] = sum_action[sum_action['Action']!="Blocked"]['count']
+
+    data = {"date":[], "allowed":[], "blocked":[], "prcnt_block":[], "total":[]}
+    for _, group in cols.groupby('date'):
+        vals = group.values
+        data['date'].append(vals[0][0])
+        data['allowed'].append(vals[0][1])
+        data['blocked'].append(vals[1][2])
+        data['prcnt_block'].append((int)(vals[1][2]/(vals[0][1]+vals[1][2]) * 100))
+        data['total'].append(vals[1][2]+vals[0][1])
+    cols = pandas.DataFrame(data)
+    
+    fig = go.Figure(data=[go.Table(
+    header=dict(values=["Date", "Total", "Allowed","Blocked","Block Rate Percentage"],
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[data['date'],data['total'],data['allowed'],data['blocked'],data['prcnt_block']],
+               fill_color='lavender',
+               align='left'))
+    ])
+
+    fig.update_layout(width=500, height=400)
+    #fig.show()
+    return fig
+
+if __name__ == "__main__":
+    build(pandas.read_csv(r"C:\Users\dtfel\OneDrive\Documents\School\Senior Project\SeniorProject\data\host_exploit_threat_stats_complete.csv"))
--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import pandas
 
 import figures.NetworkTrafficStats as nts1
 import figures.NetworkThreatStats as nts2
+import figures.AllowedThreats as nts3
+import figures.TotalNetworkStats as nts4
 
 # Import csv files into memory
 CSVs = {}
@@ -23,7 +25,9 @@ for filename in os.listdir(directory):
 
 def buildGraphs():
     # return nts1.build(CSVs["pan_allowed_traffic_stats_complete.csv"], CSVs["pan_blocked_traffic_stats_complete.csv"])
-    return nts2.build(CSVs['host_exploit_threat_stats_complete.csv'])
+    # return nts2.build(CSVs['host_exploit_threat_stats_complete.csv'])
+    # return nts3.build(CSVs['host_exploit_threat_stats_complete.csv'])
+    return nts4.build(CSVs['host_exploit_threat_stats_complete.csv'])
     
 
 app = dash.Dash()   #initialising dash app
