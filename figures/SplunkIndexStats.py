0df0515d4ddcfc397584693df0f92d06169b3627
David <dtfeldman@verizon.net>
2022-03-24 10:57:01 -0400

    added Daniel's graphs

---
+++ figures/SplunkIndexStats.py
@@ -0,0 +1,42 @@
+"""
+g. Stats for month/qtr/year per Splunk index represented as a table 
+    using allevents.stats.complete.csv
+fix: is not displaying all the indexes
+"""
+
+import pandas
+import plotly.graph_objects as go
+import plotly.subplots as sp
+import numpy as np
+
+def build(splunk_stats):
+    # Sum all counts for a given month
+    sum_action = splunk_stats.groupby(['date', 'index'], as_index=False, dropna=False)['count'].sum()
+    # print(sum_action)
+
+    # Move all required columns into a single DF and calculate any derived information
+    cols = pandas.DataFrame()
+    cols["date"] = sum_action['date']
+    cols["index"] = sum_action['index']
+    cols["count"] = sum_action['count']
+
+    data = {"date":[], "index":[], "count":[]}
+    for _, group in cols.groupby('date'):
+        vals = group.values
+        data['date'].append(vals[0][0])
+        data['index'].append(vals[0][1])
+        data['count'].append(vals[0][2])
+    cols = pandas.DataFrame(data)
+    
+    fig = go.Figure(data=[go.Table(
+    header=dict(values=["Date", "Index", "Count"],
+                fill_color='lightskyblue',
+                align='left'),
+    cells=dict(values=[data['date'],data['index'],data['count']],
+               fill_color='lavender',
+               align='left'))
+    ])
+
+    fig.update_layout(width=500, height=400)
+    #fig.show()
+    return fig
--- main.py
+++ main.py
@@ -12,10 +12,13 @@ from dash import html
 from dash import dcc
 import pandas
 
+# import graphs and table building functions
 import figures.NetworkTrafficStats as nts1
 import figures.NetworkThreatStats as nts2
-import figures.AllowedThreats as nts3
-import figures.TotalNetworkStats as nts4
+import figures.AllowedThreats as at
+import figures.TotalNetworkStats as tns
+import figures.HostExploitDetectionStats as heds
+import figures.SplunkIndexStats as sis
 
 # Import csv files into memory
 CSVs = {}
@@ -23,11 +26,12 @@ directory = 'data'
 for filename in os.listdir(directory):
     CSVs[filename] = pandas.read_csv(os.path.join(directory, filename))
 
+# Build each graph and table and return a single plotly object
 def buildGraphs():
     # return nts1.build(CSVs["pan_allowed_traffic_stats_complete.csv"], CSVs["pan_blocked_traffic_stats_complete.csv"])
     # return nts2.build(CSVs['host_exploit_threat_stats_complete.csv'])
-    # return nts3.build(CSVs['host_exploit_threat_stats_complete.csv'])
-    return nts4.build(CSVs['host_exploit_threat_stats_complete.csv'])
+    # return at.build(CSVs['host_exploit_threat_stats_complete.csv'])
+    return tns.build(CSVs['host_exploit_threat_stats_complete.csv'])
     
 
 app = dash.Dash()   #initialising dash app
